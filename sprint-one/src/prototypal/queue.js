89c9ca22a2d12a03eea9f75587e75c7b77637b1d
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-06-07 00:01:18 -0400

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.count = 0;
+  newQueue.queueCount = 0;
+  newQueue.queueFront = 1;
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.count++;
+    this.queueCount++;
+    this.storage[this.queueCount] = value;
+  },
+  dequeue: function() {
+    if (this.count > 0) {
+      this.count--;
+      this.queueFront++;
+      return this.storage[this.queueFront - 1];
+    }
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.count = 0;
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  pop: function() {
+    if (this.count > 0) {
+      this.count--;
+      return this.storage[this.count + 1];
+    }
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
