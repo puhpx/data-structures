0280dba592378bee1acd4f567017f74fc84e1f1d
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-06-07 22:05:07 -0400

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+    this.queueCount = 0;
+    this.queueFront = 1;
   }
 
+  enqueue(value) {
+    this.count++;
+    this.queueCount++;
+    this.storage[this.queueCount] = value;
+  }
+
+  dequeue() {
+    if (this.count > 0) {
+      this.count--;
+      this.queueFront++;
+      return this.storage[this.queueFront - 1];
+    }
+  }
+
+  size() {
+    return this.count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  pop() {
+    if (this.count > 0) {
+      this.count--;
+      return this.storage[this.count + 1];
+    }
+  }
+
+  size() {
+    return this.count;
+  }
 }
