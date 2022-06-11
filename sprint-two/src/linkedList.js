e9adfa2f7e31bc03f932f56c868ddcaf7883b671
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-06-11 15:10:47 -0400

    Finished first 2 functions in linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,48 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //if there is no head, then head would be tail
+    if (!list.head) {
+      list.head = list.tail = Node(value);
+      // list.head = Node(value);
+    } else {
+      var lastNode = list.head;
+      while (lastNode.next !== null) {
+        lastNode = lastNode.next;
+      }
+      list.tail = Node(value);
+      lastNode.next = list.tail;
+    }
+    //if head exists, add new node
   };
 
   list.removeHead = function() {
+    if (!list.head) {
+      return;
+    } else {
+      var rmvdHead = list.head;
+      var newHead = list.head.next;
+      list.head = newHead;
+      return rmvdHead.value;
+
+    }
   };
 
   list.contains = function(target) {
+    // if (!list.head) {
+    //   return;
+    // }
+    var temp = list.head;
+    while (temp) {
+      if (temp === target) {
+        return true;
+      } else {
+        temp = temp.next;
+      }
+
+    }
+    //start with head
+    return false;
   };
 
   return list;
