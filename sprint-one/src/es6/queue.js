222f18235b3ff276f1f59749c7835e5fb037693d
puhpx <chuckthedeveloper@gmail.com>
2022-06-11 11:36:43 -0400

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,32 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.index = 0;
   }
 
+  enqueue(value) {
+    this.index++;
+    this.storage[this.index] = value;
+  };
+
+  dequeue() {
+    var dequeuedStr = this.storage[1];
+    if (dequeuedStr === undefined) {
+      return null;
+    }
+    delete this.storage[1];
+    this.index--;
+    if (this.index > 0) {
+      for (var i = 1; i <= this.index; i++) {
+        this.storage[i] = this.storage[i+1];
+      }
+    }
+    return dequeuedStr;
+  };
+
+  size() {
+    return this.index;
+  };
 }
+
+var newQueue = new Queue;
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,29 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
+    this.index = 0;
   }
 
+  push(value) {
+    this.index++;
+    this.storage[this.index] = value;
+  };
+
+  pop() {
+    if (this.index === 0) {
+      return null;
+    }
+    var poppedStr = this.storage[this.index];
+    delete this.storage[this.index];
+    this.index--;
+    return poppedStr;
+  };
+
+  size() {
+    return this.index;
+  };
+
 }
+
+  var newStack = new Stack;
