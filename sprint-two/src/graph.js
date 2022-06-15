e95260ca3df4ff326ecf072fb9615ed7c0c1f839
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-06-14 20:14:53 -0400

    Complete Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,34 +2,64 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = [];
+  this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.nodes.includes(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i] === node) {
+      this.nodes.splice(i);
+      this.removeEdge(node);
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var j = 0; j < this.edges.length; j++) {
+    var nodeEdge = this.edges[j];
+    if (_.contains(nodeEdge, fromNode) && _.contains(nodeEdge, toNode)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  var edge = [fromNode, toNode];
+  this.edges.push(edge);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  for (var i = 0; i < this.edges.length; i++) {
+    var nodeEdge = this.edges[i];
+    if (_.contains(nodeEdge, fromNode) && _.contains(nodeEdge, toNode)) {
+      this.edges.splice(i);
+    } else {
+      if (_.contains(nodeEdge, fromNode) && toNode === undefined) {
+        this.edges.splice(i);
+      }
+    }
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this.nodes, cb);
 };
 
 /*
